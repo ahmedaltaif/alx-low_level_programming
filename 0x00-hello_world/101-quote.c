feff251c8defba770fbaac1fbb8fa61a9637a1cc
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-10 19:06:49 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return:always 0 (success))
- */
+*/
 
 int main(void)
 {
@@ -13,4 +13,3 @@ int main(void)
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout); 
 	return (1);
 }
-
