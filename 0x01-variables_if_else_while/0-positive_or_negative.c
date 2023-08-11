96c34da61b74809c9f392f4e9e098f521c11a540
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-11 19:32:34 -0400

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,12 +15,10 @@ int main(void)
     {
         printf("%d is positive\n", n);
     }
-
     if ( n == 0)
     {
         printf("%d is zero\n", n);
     }
-
     if ( n < 0)
     {
         printf("%d is negative\n", n);
