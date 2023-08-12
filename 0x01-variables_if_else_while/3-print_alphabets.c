14e0151ccab25662a6272759fe4e80a7f91b6d2c
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-11 21:51:33 -0400

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - prit alphabet low upper
+*
+* Return: Always 0
+*/
+
+
+int main(void)
+{
+
+	char hn;
+	char hm;
+	
+	for (hn = 'a' ; hn <= 'z' ; hn++)
+	{
+		putchar(hn);
+	}
+	for (hm = 'A' ; hm <= 'Z' ; hm++)
+	{
+		putchar(hm);
+	}
+	putchar('\n');
+	return (0);
+}
