bd07a6ae6adabaa63cdee66488e6a8df8bbfed86
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-11 21:57:32 -0400

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prit alphabet
+*
+* Return: Always 0
+*/
+
+
+int main(void)
+{
+
+	char hn;
+
+	for (hn = 'a' ; hn <= 'z' ; hn++)
+	{
+		if(hn != e && hn !=q)
+		putchar(hn);
+	}
+	putchar('\n');
+	return (0);
+}
