a5eb342310a44f14a94b45cb3bb60e3c3c0acaab
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-11 22:58:59 -0400

    num

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 
 	int a;
 
-	for (a = 0 ; a <= 9 ; a++)
+	for (a = '0' ; a < '10' ; a++)
 	{
 		putchar(a);
-        if (a != 9)
+        if (a != 10)
         putchar(',');
         putchar(' ');
 	}
