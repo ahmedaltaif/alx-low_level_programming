53b1e8a735cfce5a04c2af6e180286626543a798
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-11 22:53:24 -0400

    num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - print all possible numprs
+*
+* Return: Always 0
+*/
+
+
+int main(void)
+{
+
+	int a;
+
+	for (a = '0' ; a <= '9' ; num++)
+	{
+		putchar(num);
+        if (a != 9)
+        putchar(',');
+        putchar('');
+	}
+	putchar('\n');
+	return (0);
+}
