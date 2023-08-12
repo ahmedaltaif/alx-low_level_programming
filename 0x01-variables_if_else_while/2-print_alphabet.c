d72f8528c254d1b65f04f71e2ad1efcce886e7ed
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-11 21:22:21 -0400

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prit alphabet
+*
+* Return: Always 0
+*/
+
+
+int main(void) {
+
+	char n;
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
