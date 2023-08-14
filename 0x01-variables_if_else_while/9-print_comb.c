196842495afe38b583c0912b3c8ef05e2c032b1a
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-14 13:53:25 -0400

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,10 +15,10 @@ int main(void)
 	for (a = '0' ; a <= '9' ; a++)
 	{
 		putchar(a);
-        if (a != 9 )
+		if(a != 9)
 		{
-        putchar(',');
-        putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
