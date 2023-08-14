20ee85b17f00b96a9ff05b38853f99a9e6162cf0
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-14 17:26:26 -0400

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - print all possible numprs
+*
+* Return: Always 0
+*/
+
+
+int main(void)
+{
+
+	int a;
+	int b;
+
+	for (a = 0 ; a <= 8 ; a++)
+	{
+		for (b = a + 1 ; b <= 9 ; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
