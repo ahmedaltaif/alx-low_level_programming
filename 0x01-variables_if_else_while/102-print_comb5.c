ea17ef12a2308ba7649f6361559e7cf75096a690
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-14 18:46:06 -0400

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,29 +9,36 @@
 
 int main(void)
 {
-
 	int a;
 	int b;
-	
-	
+	int c;
+	int d;
 
-	for (a = 0 ; a <= 98 ; a++)
+for ( a = 0 ; a < 10 ; a++)
+{	
+
+	for (b = 0 ; b <= 10 ; b++)
 	{
-		for (b = a + 1 ; b <= 99 ; b++)
+	
+		for (c = 0 ; c < 10 ; c++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-
-			if (a != 98 && b != 99)
+			for ( d = 0 ; d < 10 ; d++)
 			{
-			putchar(',');
-			putchar(' ');
+				putchar('0' + a);
+				putchar('0' + b);
+				putchar(32);
+				putchar('0' + c);
+				putchar('0' + d);
+				if ((a != 9 && b != 8))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				d++;
 			}
 		}
 	}
+}
 	putchar('\n');
 	return (0);
 }
