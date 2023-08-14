c2d9167a64635d4de49aff55f96e30cb2405549c
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-14 18:12:23 -0400

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 	int b;
 	int c;
 
-	for(a = 0; a <= 7; a++)
+	for (a = 0; a <= 7; a++)
 	{
-		for(b = a + 1; b <= 8; b++)
+		for (b = a + 1; b <= 8; b++)
 		{
-			for(c = b + 1; c <= 9; c++)
+			for (c = b + 1; c <= 9; c++)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
