ba46b70a2d58cb74c3380eaa998b2d716b40324e
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-15 22:17:20 +0200

    alphapet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print alphabet
+*
+* Return: Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+
+}
