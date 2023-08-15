ca52e11424f21f61afe6557c41656800620d171e
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-15 23:06:09 +0200

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,14 @@
 int _islower(int c)
 {
 
-	if (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 122)
 	{
-		return (1);
+        if (c >= 66 && c <= 96)
+        {
+		    return (0);
+            else 
+            return (1);
+        }
 	}
 
 	return (0);
