74f8827258f4c3d21129b5ba27808a34bcc5abf5
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-16 00:57:53 +0200

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_last_digit - print last digit
+*
+* @d: is intered value
+*
+* Return: Always 0
+*/
+
+int print_last_digit(int d)
+{
+	int c;
+	c = d % 10; 
+
+	if (c < 0)
+	{
+		c =  -c
+	}
+	_putchar(c + '0')
+
+	return (c);
+}
