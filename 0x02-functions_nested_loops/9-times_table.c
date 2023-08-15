776cd7d86c73d7a96030c748fc4188ad63900661
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-16 01:58:11 +0200

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints table,
+ *
+ */
+void times_table(void)
+{
+	int a;
+    int h;
+    int m; 
+
+	for (a = 0; a < 10; a++)
+	{
+		for (h = 0; h < 10; h++)
+		
+			m = h * a;
+			if (h == 0)
+			{
+				_putchar(m + '0'); 
+			}
+
+			if (m < 10 && h != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m + '0');
+			} else if (m >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0'); 
+			}
+		}
+ 		_putchar('\n');
+	}
+}
