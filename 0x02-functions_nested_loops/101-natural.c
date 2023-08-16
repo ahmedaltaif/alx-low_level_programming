d34865305a615edd448e279dc1ba0fb9c7715914
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-16 03:46:05 +0200

    commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sum of all multiples of 3 or 5 up to 1024
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int a,
+    int b;
+
+	 while ( a < 1024)
+	{
+		if (( a % 3 == 0) || (a % 5 == 0))
+		{
+			b += a;
+		}
+
+		 a++;
+	}
+
+	printf("%d\n", b);
+	return (0);
+}
