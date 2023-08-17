13482ad02b2d39a21751001ab3fd4d89722f1a6f
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 01:09:01 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ * @i: intger
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+		_putchar("\n");
+	}
+
+	return (0);
+
+}
