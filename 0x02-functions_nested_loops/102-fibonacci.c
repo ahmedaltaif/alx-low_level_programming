99412cc33229c09280924c2a5257d88fb579cdb9
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-17 02:29:22 +0200

    commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,28 +2,31 @@
 
 /**
 * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
-* 
+*
 * Return: Always 0
 */
 
 int main(void)
 {
-    int i;
+	int i;
 	unsigned long  a = 0;
 	unsigned long  b = 1;
 	unsigned long  c;
 
-	for (i = 1; i <= 50; i++)
-		{	
+	for (i = 1; i <= 49; i++)
+		{
+			c = a + b;
 			
-    		c = a + b; 
 			printf ("%ld", c);
+
 			a = b;
 			b = c;
+
 			if (i == 49)
 			{
 				printf("\n");
 			}
+            
 			else 
 			{
 				printf (", ");
