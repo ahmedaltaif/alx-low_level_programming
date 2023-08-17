4a9c02f541e9c79edcf6d2f845c49dd647a38e95
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 01:45:26 +0200

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers -  prints the numbers, from 0 to 9
+ * print_most_numbers -  prints the numbers, from 0 to 9
  *
  * Return: Always 0.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int a;
 
