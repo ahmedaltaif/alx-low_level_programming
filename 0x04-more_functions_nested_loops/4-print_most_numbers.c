26b0ed5c9bad0b20ab06a50ba9445af0d766d1b6
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 01:37:59 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if ((a != '2') || (a != '4'))
+		_putchar(a);
+	}
+	_putchar('\n');
+
+
+}
