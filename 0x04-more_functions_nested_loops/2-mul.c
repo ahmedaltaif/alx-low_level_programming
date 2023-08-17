0ded101d30c0242a2ec821e14cc6822214160b2a
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 00:16:36 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul -  multiplies two integers.
+ * @a: intger
+ * @b: intger
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int sum = a * b;
+    
+	return (sum);
+
+}
