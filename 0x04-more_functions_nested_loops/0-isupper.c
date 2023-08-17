f6dc6dc130ac5be3035f9ffd7bfc398eccafeacc
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-17 23:46:29 +0200

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,16 +8,9 @@
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		if ((c >= 'a') && (c <= 'z'))
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+	else
+		return (1);
 
 }
