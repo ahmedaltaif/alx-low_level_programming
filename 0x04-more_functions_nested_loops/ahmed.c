64ffb126d187ba0444be567af12170086a17b64c
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 02:10:10 +0200

    commit

--- 0x04-more_functions_nested_loops/ahmed.c
+++ 0x04-more_functions_nested_loops/ahmed.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers -  prints the numbers, from 0 to 9
@@ -6,7 +6,7 @@
  * Return: Always 0.
  */
 
-void more_numbers(void)
+void main(void)
 {
 	int a;
 	int i;
