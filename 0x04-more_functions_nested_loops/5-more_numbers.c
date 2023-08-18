76c29c24b70bca28701db586703b522525929623
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 02:22:55 +0200

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers -  prints the numbers, from 0 to 9
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  *
  * Return: Always 0.
  */
@@ -9,17 +9,16 @@
 void more_numbers(void)
 {
 	int a;
-	int i;
+	int b;
 
-	for (i = '1'; i <= '10'; i++)
+	for (a = 1; a <= 10; a++)
 	{
-		for (a = '0'; a <= '14'; a++)
+		for (b = 0; b <= 14; b++)
 		{
-			if (a >= 10)
+			if (b >= 10)
 			_putchar('1');
-			_putchar(a % 10 + '0');
+			_putchar(b % 10 + '0');
 		}
-	_putchar('\n');
-	}
-
+			_putchar('\n');
+		}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,11 +4,12 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+
 #endif /* MAIN_H */
