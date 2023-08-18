05c6e26a5e7aa955ad0c85007009f49b07158e7d
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-18 02:35:28 +0200

    commmit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+_	putchar('\n');
+}
