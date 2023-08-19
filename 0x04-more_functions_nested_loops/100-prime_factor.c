d637e5e4e0a03a4f55f078d5b8f4537e33bbdb39
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-19 11:19:44 +0200

    commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,14 +3,14 @@
 
 /**
  * main -  prime factor of 612852475143
- * 
+ *
  * Return: always 0
  */
 
 int main(void)
 {
 	unsigned long int a;
-    unsigned long int b = 612852475143;
+	unsigned long int b = 612852475143;
 
 	for (a = 3; a < 782849; a = a + 2)
 	{
