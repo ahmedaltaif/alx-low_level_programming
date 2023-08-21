60806411b6221f7b4a8e1ab29b88de0a17e56e26
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-22 01:09:00 +0200

    pointer task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int - to swap int valu
+*
+*
+* @a: intger
+* @b: intger
+*/
+void swap_int(int *a, int *b)
+{
+	int aa = *a;
+	int bb = *b;
+
+	*a = bb;
+	*b = aa;  
+}
