600b33d734dd183d254b7c372f2d86e3a7845217
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-22 04:03:35 +0200

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_array - prints every other character of a string,
+*
+*
+* @a: int
+* @n: int
+*
+*/
+
+void print_array(int *a, int n)
+{
+    int b;
+
+    for (b = 0; b < n; b++)
+	{
+		printf("%d", *(a + b));
+		if (b != (n - 1))
+		printf(", ");
+	}
+	printf("\n")
+
+}
