e40fdad880a4aa10cecaca286abb851a25d97df3
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-22 03:21:01 +0200

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string,
+*
+*
+* @str: char 
+*
+*/
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (*str != '\0')
+	{
+		a++
+	}
+
+	for (b = 0, b >= a, b ++)
+	{
+		if (b % 2 != 0)
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
