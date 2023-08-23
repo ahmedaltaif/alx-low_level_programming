51594ffced0ae3dfbfed2dd1c33758be5ffa8257
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-23 02:06:25 +0200

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+* puts_half -  prints half of a string,
+*
+*
+* @str: string
+*
+*/
+
+
+void puts_half(char *str)
+{
+
+	int a = 0;
+	int i;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	
+	if ((a % 2) == 0 )
+	{
+		for (i = a/2; i < a; i++)
+		{
+		_putchar(str[i]);
+		
+
+		}
+
+	}
+	else
+	{
+		for ((i = ((a-1)/2)); i < a; i++)
+		{
+		_putchar(str[i]);
+		
+
+		}
+	}
+	_putchar('\n');
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,7 +11,8 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-void print_array(int *a, int n);
+void print_array(int *a, int n);    
+void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 
 
