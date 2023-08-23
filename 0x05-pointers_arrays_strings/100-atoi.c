850915afc8e85c6a3a5137945fe04b6febb10c82
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-23 03:25:09 +0200

    pointers and arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,24 +12,29 @@ int _atoi(char *s)
 	int m = 0;
 	int sum = 0;
 	int b = 0;
+
 	m = -1;
 
 	while (s[i] != '\0')
 	{
+		
 		if (s[i] == '-')
 		{
 				m *= -1;
 		}
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 				sum *= 10;
 				sum  -= (s[i] - '0');
 				b = 1;
 		}
+
 		else if (b == 1)
 		{
 				break;
 		}
+
 		i++;
 	}
 	sum *= m;
