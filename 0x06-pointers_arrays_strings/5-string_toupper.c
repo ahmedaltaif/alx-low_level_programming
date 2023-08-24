4e343d33aad57bd47f7a3694553a44bc89310b50
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-08-25 01:56:10 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* *string_toupper -  changes all lowercase letters of a string to uppercase..
+*
+* @str: array
+*
+* Return: str
+*/
+char *string_toupper(char *str)
+{
+
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (str[a] >= 'a' && str[a] <= 'z')
+		{
+			str[a] = str[a] - 32;
+		}
+	}
+
+	return (str);
+}
