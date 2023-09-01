f8b866d37610dbeb71532724e3e0ef0a1b6435d8
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-02 01:36:28 +0200

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: num of arg
+ * @argv: array of arg
+ *
+ * Return:always 1 (success))
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int a;
+	a = argc - 1 ;
+	printf("%d\n", a);
+	return (0);
+}
