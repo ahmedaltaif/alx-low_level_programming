b9930448d2b196ad809ba0df362da1db30af0ace
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-06 03:52:03 +0200

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int x;
 	int y;
 
-	s = malloc(sizeof(int) * height);
+	s = (int **)malloc(sizeof(int) * height);
 
 	if (width <= 0 || height <= 0 || s == NULL)
 	{
