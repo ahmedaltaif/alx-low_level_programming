0cea09318b40f0b38add0e8fa0ab5c6c9bcee3aa
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-13 10:44:04 +0200

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,26 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name : %s\n", d->name);
-		printf("Age : %.6f\n", d->age);
-		printf("Owner : %s\n", d->owner);
+		if (d->name == NULL)
+		{
+			printf("Name : (nil)\n");
+		}else
+		{
+			printf("Name : %s\n", d->name);
+		}
+		if (d->age == 0)
+		{
+			printf("Name : (nil)\n");
+		}else
+		{
+			printf("Age : %.6f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Name : (nil)\n");
+		}else
+		{
+			printf("Owner : %s\n", d->owner);
+		}
 	}
 }
