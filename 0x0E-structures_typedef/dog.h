584b045dc3764a250bb5aa0824d80c689516d45f
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-13 09:55:39 +0200

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
