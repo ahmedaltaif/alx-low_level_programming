968dd28794bca0dcc4e6d242556776aad6ed56a6
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-13 20:41:44 +0200

    commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog 
+ * @d: pointer 
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+		free(d->owner);
+		free(d->name);
+	}
+}
