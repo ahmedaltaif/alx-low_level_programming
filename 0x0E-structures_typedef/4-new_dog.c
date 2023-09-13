98dde09b8f1003915b232fd23bae3a3076dfc5e8
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-13 20:29:23 +0200

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 	s = malloc(sizeof(dog_t));
 	if (s == NULL)
+	{
+		free (s);
 		return (NULL);
+	}
 	s->name = malloc((lenna + 1) * sizeof(char));
 	if (name == NULL)
 	{
