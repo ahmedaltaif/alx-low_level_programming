e6dfa06af9b49520427eb8887a546775b6bf5ecf
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-27 09:52:24 +0200

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,13 @@ size_t print_listint(const listint_t *h)
 {
 	int i = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		++i;
 	}
-	return(i);
+	return (i);
 }
