26a1202097c8c912e44a64436a82a9b2a1b931a7
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-10-21 08:25:35 +0200

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,25 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 void error_file(int file_from, int file_to, char *argv[]);
+void c_elf(int elf);
+void p_entry(unsigned long int _entry, unsigned char *e_entry);
+void ptype(unsigned int e_type, unsigned char *e_entry);
+void pabi(unsigned char *e_entry);
+void posabi(unsigned char *e_entry);
+void pversion(unsigned char *e_entry);
+void pmagic(unsigned char *e_entry);
+void pdata(unsigned char *e_entry);
+void pclass(unsigned char *e_entry);
+void pcheck(unsigned char *e_entry);
+
+
+
+
+
+
+
+
+
+
 
 #endif
