9f4b272b106210e02f9a11934996167b52f3af2a
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-10-21 02:21:59 +0200

    commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @text_content: content
+ * @filename: filename
+ * Return: 1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int a;
+	int len;
+	int write;
+
+	if (!filename)
+		return (-1);
+	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (a == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	while (text_content[len])
+	{
+		len++
+	}
+	write = write(a, text_content, len);
+
+	if (write == -1)
+	{
+		return (-1);
+	}
+	close(a);
+	return (1);
+}
