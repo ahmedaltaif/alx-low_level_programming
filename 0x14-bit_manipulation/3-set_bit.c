c46203f06dd5dd972e75e48dc8741946299e5331
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-10-26 21:12:36 +0200

    binary_to unit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * set_bit - function sets value of a bit to 1 at a given index
+ * @n: number.
+ *
+ * Return: 1 if it worked, or -1 if an error
+ *
+ * @index: starting from 0 of the bit you want to set.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+		if (index > 63)
+			return (-1);
+
+		int value;
+
+		value = 1 << index;
+
+		*n = *n | value;
+
+		return (1);
+
+}
