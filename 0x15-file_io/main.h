2cbc74d05aa42bda5a65fc139748898c39f9a064
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-10-31 00:32:54 +0200

    cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,7 +11,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
-void error_file(int file_from, int file_to, char *argv[]);
+void _error(int file_from, int file_to, char *argv[]);
 
 
 #endif
