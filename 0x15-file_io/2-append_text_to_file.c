38b9c0d5d2ed91dc2aaa3c8b7164dcb03a9d9443
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-10-31 00:16:47 +0200

    append_text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 		while (text_content[tall] != '\0')
 		{
-			tall++
+			tall++;
 		}
 		reed = write(fil, text_content, tall);
 		if (reed < 0)
